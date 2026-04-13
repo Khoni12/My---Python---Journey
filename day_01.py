43333e77666cb1db5f01321055de89a5feebeb32
Khoni M <khonimphuthi@gmail.com>
2026-04-13 23:49:12 +0200

    Create day_01.py

---
+++ day_01.py
@@ -0,0 +1,26 @@
+# 30 days of python - day 01
+# Date: 13/04/2026
+# Exercise Level 1 - Arithmetic Operations
+print (3 + 4)                  # Addition: 7
+print (3 - 4)                  # subtraction: -1
+print (3 * 4)                  # multiplication: 12
+print (3 / 4)                  # Division: 0.75
+print (3 ** 4)                 # exponential: 81
+print (3 % 4)                  # modulus: 3
+print (3 // 4)                 # Floor division: 0 
+
+# Exercise Level 2 - Strings 
+print ("Khoni")                # First name 
+print ("mphuthi")              # Family name
+print ("south africa")         # country 
+print("i am enjoying 30 days of python")  # Statement 
+
+# Exercise Level 3 - checking Data Types 
+print(type(10))                # int
+print(type(3.14))              # float 
+print(type(1 + 3j))            # complex
+print(type("khoni"))           # str
+print(type([1, 2, 3]))         # list 
+print(type({"name";"khoni"}))  # dict
+print(type({9.8, 3.14, 2.7}))  # set
+print(type((9.8, 3.14, 2.7)))  # tuple
