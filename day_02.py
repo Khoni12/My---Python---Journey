19b7a631f39edbc042d284fbfa34a67a3e53bf3c
Khoni M <khonimphuthi@gmail.com>
2026-04-14 22:15:16 +0200

    Create day_02.py

---
+++ day_02.py
@@ -0,0 +1,32 @@
+# Day 02: 30 Days of Python Progamming 
+# Exercise 1: Declare Variables 
+first_name = "Khoni"
+last_name = "Mpuhthi"
+full_name = First_name + "   " + last_name
+country = "south Africa"
+city = "Bloemfontein"
+age = 21
+year = 2026
+is_married = False
+is_true = True
+is_light_on = True
+
+# Print all variables 
+print(first_name)
+print(last_name)
+print(full_name)
+print(country)
+print(city)
+print(age)
+print(year)
+print(is_married)
+
+# Exercise 2: Check data types
+print(type(first_name))          # <class  'str'>
+print(type(age))                 # <class  'int'>
+print(type(is_married))          # <class  'bool'>
+
+# Exercise 3: Built-in functions
+print(len(first_name))           # length of your name
+print(min(20, 30, 40))           # smallest number 
+print(max(20, 30, 40))           # biggest number 
