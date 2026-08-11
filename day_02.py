662127c8c234e1a7519905bb039a9bbe5789a172
Khoni Mphuthi <khonimphuthi@gmail.com>
2026-08-11 11:12:34 -0700

    Add examples of lists with various data types
    Added examples of lists with different data types.

--- day_02.py
+++ day_02.py
@@ -32,6 +32,7 @@ print(min(20, 30, 40))           # smallest number
 print(max(20, 30, 40))           # biggest number 
 
 [0, 1, 2, 3, 4, 5]  # all are the same data types - a list of numbers
+
 ['Banana', 'Orange', 'Mango', 'Avocado'] # all the same data types - a list of strings (fruits)
 ['Finland','Estonia', 'Sweden','Norway'] # all the same data types - a list of strings (countries)
 ['Banana', 10, False, 9.81] # different data types in the list - string, integer, boolean and float
