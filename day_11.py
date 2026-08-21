4735578ae96731d496735164090de32201aad2df
Khoni Mphuthi <khonimphuthi@gmail.com>
2026-08-21 18:49:49 +0200

    Pandas: Added DataFrame example using pandas
    Added example of creating a DataFrame from a dictionary.

--- day_11.py
+++ day_11.py
@@ -29,3 +29,33 @@ print(Bricks)
  IN         india  new delhi   3.286     1252.00
  CH         china    beijing   9.597     1357.00
  SA  south africa   pretoria   1.221       52.98
+
+# DataFrame for CSV File
+# eg. brics.csv   (CSV - Comma Separated Values)
+
+#### example 2
+
+# Pre-defined lists
+names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
+dr =  [True, False, False, False, True, True, True]
+cpc = [809, 731, 588, 18, 200, 70, 45]
+
+# Import pandas as pd
+import pandas as pd
+
+# Create dictionary my_dict with three key:value pairs: my_dict
+my_dict = {'country':names, 'drives_right': dr, 'cars_per_cap': cpc}      ### here we dont put quotes on tha values because they represent values(recocnised vaules.)
+
+# Build a DataFrame cars from my_dict: cars
+cars = pd.DataFrame(my_dict)
+print(cars)
+
+                  ### answers
+  country  drives_right  cars_per_cap
+0  United States          True           809
+1      Australia         False           731
+2          Japan         False           588
+3          India         False            18
+4         Russia          True           200
+5        Morocco          True            70
+6          Egypt          True            45
