d7faa40cc3694c0895f3f1b4e8a936f337b1c882
Khoni Mphuthi <khonimphuthi@gmail.com>
2026-08-21 18:20:48 +0200

    Pandas
    Added custom index labels to the DataFrame for better readability.

--- day_11.py
+++ day_11.py
@@ -7,6 +7,7 @@ dict = {
     "capital": ["brazilia", "moscow", "new delhi", "beijing", "pretoria"],
     "area": [8.516, 17.10, 3.286, 9.597, 1.221],
     "population": [200.4, 143.5, 1252, 1357, 52.98]}
+
 Bricks = pd.DataFrame(dict)
 print(Bricks)
 
@@ -17,3 +18,14 @@ print(Bricks)
 2         india  new delhi   3.286     1252.00
 3         china    beijing   9.597     1357.00
 4  south africa   pretoria   1.221       52.98
+
+Bricks.index = ["BR", "RU", "IN", "CH", "SA"]              # This will change the numbers to being labeled
+print(Bricks)
+
+                  ### ANSWERS
+       country    capital    area  population
+ BR        brazil   brazilia   8.516      200.40
+ RU        russia     moscow  17.100      143.50
+ IN         india  new delhi   3.286     1252.00
+ CH         china    beijing   9.597     1357.00
+ SA  south africa   pretoria   1.221       52.98
